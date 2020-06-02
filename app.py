8ce646253e055f1d0d02f460f6c301b50e49c4c4
Biswas, Avhishek <avhishek.biswas@und.edu>
2020-06-02 22:56:22 +0000

    Added Data Input

--- app.py
+++ app.py
@@ -15,6 +15,17 @@ def main():
     st.markdown("Are your Mushroom Edible or Poisonous")
     st.sidebar.markdown("Are your Mushroom Edible or Poisonous")
     
+    
+    @st.cache(persist=True)
+    def load_data():
+        data = pd.read_csv('mushrooms.csv')
+        label = LabelEncoder()
+        
+        for col in data.columns:
+            data[col] = label.fit_transform(data[col])    
+        return data
+    
+    df = load_data()
 
 
 
